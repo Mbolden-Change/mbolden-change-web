0ccafdbc376588f5eea075329c9f59bfcd30ff61
Maazin Sherif <maazinsherif@Maazins-Macbook.local>
2025-10-16 08:36:15 +0800

    add: email fields to richtext

--- frontend/components/blocks/RichText.tsx
+++ frontend/components/blocks/RichText.tsx
@@ -8,16 +8,27 @@ import styles from './RichText.module.css';
 type RichTextProps = {
   title: string;
   text: PortableTextBlock[];
+  email: string;
+  emailMessage: string;
   variant?: 'standalone' | 'nested';
 };
 
-export default function RichText({ title, text, variant = 'standalone' }: RichTextProps) {
+export default function RichText({ title, text, email, emailMessage, variant = 'standalone' }: RichTextProps) {
   const variantClass = variant === 'standalone'? styles.standalone: styles.nested;
 
     const content = (
       <div className={variantClass}>
         {title && <Headline tag='h2' text={title} className={styles.headline}/>}
         {<PortableTextComponent value={text} />}
+        {email &&
+          <a
+            href={`mailto:${email}`}
+            aria-label="Email"
+            className={styles.emailMsg}
+          >
+            {emailMessage}
+          </a>
+        }
       </div>
     );
 
